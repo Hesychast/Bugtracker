020ac28ff186a8c85c30063a66e66c8d738eda1d
Hesychast <wdvlpr@gmail.com>
2023-01-10 16:31:37 +0200

    First commit

---
+++ bugtracker.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Bugtracker
+ * Plugin URI:      https://github.com/Hesychast/Bugtracker
+ * Description:     Bugtracker application with specific usage requirements.
+ * Author:          Hesychast
+ * Author URI:      https://github.com/Hesychast
+ * Text Domain:     bugtracker
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Bugtracker
+ */
+
